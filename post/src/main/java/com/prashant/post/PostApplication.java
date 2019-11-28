87c58208385799a4c38b59a57892b7e43463b566
prashant <prashant.hariharan@externals.adidas.com>
2019-11-28 17:37:11 +0100

    Restructuring

--- post/src/main/java/com/prashant/post/PostApplication.java
+++ post/src/main/java/com/prashant/post/PostApplication.java
@@ -3,6 +3,7 @@ package com.prashant.post;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -10,6 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @ComponentScan(basePackages = {"com.prashant.post"})
 @EntityScan(basePackages = "com.prashant.post.domain")
 @EnableJpaRepositories(basePackages = "com.prashant.post.repo")
+@EnableDiscoveryClient
 // To enable the microservice to be discovered within kubernetes
 public class PostApplication {
 
